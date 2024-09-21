2981b143e49a7ce762f03aff9b74b378b58e62fc
sanjanachj <sanjanachoudhary717@gmail.com>
2024-09-21 19:33:17 +0530

    Add files via upload

---
+++ finding largest num among 3 and 4.py
@@ -0,0 +1,24 @@
+# for largest no. among 3 numbers.
+num1=int(input("enter num 1="))
+num2=int(input("enter num 2="))
+num3=int(input("enter num 3="))
+if(num1>num2 and num1>num3):
+ print("num1 is gretest",num1)
+elif(num2>num3):
+ print("num2 is gretest",num2)
+else:
+ print("num3 is gretest",num3)
+# for largest no. among 4 numbers.
+'''
+a=int(input("a="))
+b=int(input("b="))
+c=int(input("c="))
+d=int(input("d="))
+if(a>b and a>c and a>d):
+    print("a is greter")
+elif(b>c and b>d):
+    print("b is greter")
+elif(c>d):
+    print("c is greter")
+else:
+    print("d is greter") '''
